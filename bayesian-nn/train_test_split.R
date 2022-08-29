0ca246fda502fe2f42e20ef654b267974ec06d2b
Xudong Tang <44549914+XudongTang@users.noreply.github.com>
2022-08-29 16:34:59 -0500

    generalized code and added comment

--- bayesian-nn/train_test_split.R
+++ bayesian-nn/train_test_split.R
@@ -1,74 +1,46 @@
 library("creditmodel")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-P1_1 <- read.csv(file = './P1_1SP.csv', header = FALSE)
-P1_2 <- read.csv(file = './P4_4SP.csv', header = FALSE)
-#P1_3 <- read.csv(file = './P1_3.csv', header = FALSE)
-#P1_4 <- read.csv(file = './P1_4.csv', header = FALSE)
+level = "Phylum"
 
+the_one_that_do_the_work <- function(level) {
+  path = paste("../processed-data/",level, "/full-data", sep = "")
+  file_name = dir(path)
+  path_name = dir(path, full.names = TRUE)
+  all_files = lapply(path_name, read.csv, header = FALSE)
+  num = length(file_name)
+  
+  for (i in 1:num) {
+    split(all_files[[i]], level, file_name[i])
+  }
+}
 
-a <- train_test_split(
-  P1_1, 
-  prop = 0.8,
-  split_type = "Random",
-  occur_time = NULL,
-  cut_date = NULL,
-  start_date = NULL,
-  save_data = FALSE,
-  dir_path = tempdir(),
-  file_name = NULL,
-  note = FALSE,
-  seed = 42
-)
+split <- function(full_data, level, filename){
+  name = substr(filename, 1, nchar(filename) - 4)
+  split_data <- train_test_split(
+    full_data, 
+    prop = 0.8,
+    split_type = "Random",
+    occur_time = NULL,
+    cut_date = NULL,
+    start_date = NULL,
+    save_data = FALSE,
+    dir_path = tempdir(),
+    file_name = NULL,
+    note = FALSE,
+    seed = 42
+  )
+  
+  fullpath_train = paste("../processed-data/", level, 
+                   "/train-test-split/", name, "_train.csv",
+                   sep = "")
+  fullpath_test = paste("../processed-data/", level, 
+                         "/train-test-split/", name, "_test.csv",
+                         sep = "")
+  write.table(split_data[2]$train, file = fullpath_train, 
+              row.names = FALSE, col.names = FALSE, sep = ",")
+  write.table(split_data[1]$test, file = fullpath_test, 
+              row.names = FALSE, col.names = FALSE, sep = ",")
+}
 
-b <- train_test_split(
-  P1_2, 
-  prop = 0.8,
-  split_type = "Random",
-  occur_time = NULL,
-  cut_date = NULL,
-  start_date = NULL,
-  save_data = FALSE,
-  dir_path = tempdir(),
-  file_name = NULL,
-  note = FALSE,
-  seed = 42
-)
-
-c <- train_test_split(
-  P1_3, 
-  prop = 0.8,
-  split_type = "Random",
-  occur_time = NULL,
-  cut_date = NULL,
-  start_date = NULL,
-  save_data = FALSE,
-  dir_path = tempdir(),
-  file_name = NULL,
-  note = FALSE,
-  seed = 42
-)
-
-d <- train_test_split(
-  P1_4, 
-  prop = 0.8,
-  split_type = "Random",
-  occur_time = NULL,
-  cut_date = NULL,
-  start_date = NULL,
-  save_data = FALSE,
-  dir_path = tempdir(),
-  file_name = NULL,
-  note = FALSE,
-  seed = 42
-)
-
-
-P1_1 <- rbind(a[2]$train,a[1]$test)
-P1_2 <- rbind(b[2]$train,b[1]$test)
-P1_3 <- rbind(c[2]$train,c[1]$test)
-P1_4 <- rbind(d[2]$train,d[1]$test)
-
-write.csv(P1_1, file = './P1_1SP.csv', row.names = FALSE)
-write.csv(P1_2, file = './P4_4SP.csv', row.names = FALSE)
-write.csv(P1_3, file = './P1_3.csv', row.names = FALSE)
-write.csv(P1_4, file = './P1_4.csv', row.names = FALSE)
+the_one_that_do_the_work("Phylum")
