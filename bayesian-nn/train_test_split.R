e10a3014260c14c6701b870c0af936b3d068120f
Xudong Tang <44549914+XudongTang@users.noreply.github.com>
2022-08-30 19:44:00 -0500

    added full comment and script for jobs in CHTC

--- bayesian-nn/train_test_split.R
+++ bayesian-nn/train_test_split.R
@@ -1,22 +1,34 @@
 library("creditmodel")
+# set the working directory to where the current script is located
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-level = "Phylum"
-
+# This function reads all data (normalization+responses) within a level
+# then split the data into a training set and a testing set.
+# It then saves the 2 sets in 2 seperate CSVs
 the_one_that_do_the_work <- function(level) {
+  # set the path to the directory where the data is located
   path = paste("../processed-data/",level, "/full-data", sep = "")
+  # get all the file names in the directory
   file_name = dir(path)
+  # read all files and save into a data frame
   path_name = dir(path, full.names = TRUE)
   all_files = lapply(path_name, read.csv, header = FALSE)
+  # number of datasets for this level
   num = length(file_name)
   
+  # for each file, split and save
   for (i in 1:num) {
     split(all_files[[i]], level, file_name[i])
   }
 }
 
+# This function split the dataset into training and testing sets
+# and then save them in 2 CSVs
 split <- function(full_data, level, filename){
+  # trim the .csv from the original file name for naming's sake
   name = substr(filename, 1, nchar(filename) - 4)
+  # Split the data using 80% as training and the rest as testing
+  # the seed is 42 to match Rosa's split
   split_data <- train_test_split(
     full_data, 
     prop = 0.8,
@@ -30,17 +42,19 @@ split <- function(full_data, level, filename){
     note = FALSE,
     seed = 42
   )
-  
+  # name the training and testing CSVs
   fullpath_train = paste("../processed-data/", level, 
                    "/train-test-split/", name, "_train.csv",
                    sep = "")
   fullpath_test = paste("../processed-data/", level, 
                          "/train-test-split/", name, "_test.csv",
                          sep = "")
+  # Write the 2 sets to 2 files
   write.table(split_data[2]$train, file = fullpath_train, 
               row.names = FALSE, col.names = FALSE, sep = ",")
   write.table(split_data[1]$test, file = fullpath_test, 
               row.names = FALSE, col.names = FALSE, sep = ",")
 }
 
+# One only need to specify the level of the dataset to make everything works
 the_one_that_do_the_work("Phylum")
