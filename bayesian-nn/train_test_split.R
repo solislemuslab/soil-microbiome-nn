ac1943512ba3a64df81bb68add78065d941c04ea
Xudong Tang <xtang75@wisc.edu>
2022-10-26 15:32:09 -0500

    changes

--- bayesian-nn/train_test_split.R
+++ bayesian-nn/train_test_split.R
@@ -158,11 +158,53 @@ split_other <- function(full_data, type, filename){
               row.names = FALSE, col.names = FALSE, sep = ",")
 }
 
+all_otu_split <- function(level, aug) {
+  path = paste("../processed-data/otu_data/all_otu/", aug, "/"
+               , level, "/full-data", sep = "")
+  file_name = dir(path)
+  path_name = dir(path, full.names = TRUE)
+  all_files = lapply(path_name, read.csv, header = FALSE)
+  num = length(file_name)
+  
+  for (i in 1:num) {
+    split_all_otu(all_files[[i]], level, file_name[i], aug)
+  }
+}
+
+split_all_otu <- function(full_data, level, filename, aug){
+  name = substr(filename, 1, nchar(filename) - 4)
+  split_data <- train_test_split(
+    full_data, 
+    prop = 0.8,
+    split_type = "Random",
+    occur_time = NULL,
+    cut_date = NULL,
+    start_date = NULL,
+    save_data = FALSE,
+    dir_path = tempdir(),
+    file_name = NULL,
+    note = FALSE,
+    seed = 42
+  )
+  
+  fullpath_train = paste("../processed-data/otu_data/all_otu/", aug, "/", level
+                         , "/train-test-split/", name, "_train.csv", sep = "")
+  fullpath_test = paste("../processed-data/otu_data/all_otu/", aug, "/", level
+                        , "/train-test-split/", name, "_test.csv", sep = "")
+  
+  write.table(split_data[2]$train, file = fullpath_train, 
+              row.names = FALSE, col.names = FALSE, sep = ",")
+  write.table(split_data[1]$test, file = fullpath_test, 
+              row.names = FALSE, col.names = FALSE, sep = ",")
+}
+
+
+
 levels <- cbind("Phylum", "Class", "Order", "Family", "Genus")
 
 # One only need to specify the level of the dataset to make everything works
 for (i in 1:length(levels)){
-  for (j in 1:3) {
+  for (j in 0:3) {
     otu_split(levels[i], j)
   }
 }
@@ -173,3 +215,11 @@ for (i in 1:length(levels)){
 
 other_split("disease_suppression_data")
 other_split("soil_chemistry_data")
+
+levels <- cbind("Phylum", "Class", "Order", "Family")
+for (i in 1:length(levels)){
+  all_otu_split(levels[i], "augumented")
+}
+for (i in 1:length(levels)){
+  all_otu_split(levels[i], "non-augumented")
+}
